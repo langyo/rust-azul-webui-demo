8cb295f0b597244d37e87bda1483eb2686ad41e7
伊欧 <langyo.china@gmail.com>
2024-02-13 11:23:04 +0800

    💄

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use azul::prelude::*;
-use azul::widgets::Button;
 
 struct DataModel {
     counter: usize,
@@ -11,25 +10,80 @@ extern "C" fn layout_func(data: &mut RefAny, _: &mut LayoutCallbackInfo) -> Styl
         None => return StyledDom::default(),
     };
 
-    let mut label = Dom::text(counter);
-    label.set_inline_style("font-size: 2em");
-    label.set_inline_style("color: green");
+    let label =
+        Dom::text(counter).with_inline_css_props(NodeDataInlineCssPropertyVec::from_vec(vec![
+            NodeDataInlineCssProperty::Normal(CssProperty::FontSize(StyleFontSizeValue::Exact(
+                StyleFontSize {
+                    inner: PixelValue::em(2.0),
+                },
+            ))),
+            NodeDataInlineCssProperty::Normal(CssProperty::TextColor(StyleTextColorValue::Exact(
+                StyleTextColor {
+                    inner: ColorU {
+                        r: 0,
+                        g: 255,
+                        b: 0,
+                        a: 255,
+                    },
+                },
+            ))),
+        ]));
 
-    let mut button = Button::new("+1");
-    button.set_on_click(data.clone(), handle_on_click);
-    let mut button = button.dom();
-    button.set_inline_style("flex-grow: 1");
+    let button = Dom::div()
+        .with_inline_css_props(NodeDataInlineCssPropertyVec::from_vec(vec![
+            NodeDataInlineCssProperty::Normal(CssProperty::BackgroundContent(
+                StyleBackgroundContentVecValue::Exact(StyleBackgroundContentVec::from_vec(vec![
+                    StyleBackgroundContent::Color(ColorU {
+                        r: 0,
+                        g: 0,
+                        b: 255,
+                        a: 255,
+                    }),
+                ])),
+            )),
+            NodeDataInlineCssProperty::Normal(CssProperty::MarginTop(LayoutMarginTopValue::Exact(
+                LayoutMarginTop {
+                    inner: PixelValue::px(8.0),
+                },
+            ))),
+            NodeDataInlineCssProperty::Normal(CssProperty::MarginLeft(
+                LayoutMarginLeftValue::Exact(LayoutMarginLeft {
+                    inner: PixelValue::px(8.0),
+                }),
+            )),
+            NodeDataInlineCssProperty::Normal(CssProperty::Width(LayoutWidthValue::Exact(
+                LayoutWidth {
+                    inner: PixelValue::px(64.0),
+                },
+            ))),
+            NodeDataInlineCssProperty::Normal(CssProperty::Height(LayoutHeightValue::Exact(
+                LayoutHeight {
+                    inner: PixelValue::px(64.0),
+                },
+            ))),
+        ]))
+        .with_callback(
+            EventFilter::Hover(HoverEventFilter::MouseUp),
+            data.clone(),
+            handle_on_click,
+        )
+        .with_children(DomVec::from_vec(vec![label]));
 
     Dom::body()
-        .with_child(label)
         .with_child(button)
+        .with_callback(
+            EventFilter::Window(WindowEventFilter::TextInput),
+            data.clone(),
+            handle_on_click,
+        )
         .style(Css::empty())
 }
 
 extern "C" fn handle_on_click(data: &mut RefAny, _: &mut CallbackInfo) -> Update {
+    println!("+1");
     let mut data = match data.downcast_mut::<DataModel>() {
         Some(s) => s,
-        None => return Update::DoNothing, // error
+        None => return Update::DoNothing,
     };
 
     data.counter += 1;
